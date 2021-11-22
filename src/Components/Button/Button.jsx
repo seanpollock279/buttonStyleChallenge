cbe5a798b87d85533b7a06f8466f494b8614a3cf
Sean Pollock <seanpollock@Seans-Mac-mini.local>
2021-11-22 16:56:39 -0500

    first commit

---
+++ src/Components/Button/Button.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './styles.css';
+
+const STYLES = [
+    "default",
+    "outline",
+    "text",
+    "disabled"
+]
+
+const SIZES = [
+    "md",
+    "sm",
+    "lg"
+]
+
+export default function Button( {children, type, onClick, buttonStyle, buttonSize }) {
+
+    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
+    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+
+    return (
+        <div>
+            <button className={checkButtonStyle} onClick={onClick}>
+                {children}
+            </button>
+        </div>
+    )
+}
+
